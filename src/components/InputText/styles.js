dce06e8d1897e86a41afa5a90d576eb93cc02692
BeAmarawn <bernardoamaralmarinho@gmail.com>
2021-01-31 14:18:26 -0300

    feat: add input short text component

---
+++ src/components/InputText/styles.js
@@ -0,0 +1,13 @@
+import styled from 'styled-components/native';
+import {Input} from 'react-native-elements';
+
+export const InputShortText = styled(Input)``;
+export const Container = styled.View`
+  padding-top: 10px;
+  margin-bottom: 15px;
+  margin-right: 10px;
+  margin-left: 10px;
+  border-width: 1px;
+  border-color: #a75df2;
+  border-radius: 15px;
+`;
