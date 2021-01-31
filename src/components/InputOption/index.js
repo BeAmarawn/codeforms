e9a9d6d237060c0335f14238e7df18382d7ee1a1
BeAmarawn <bernardoamaralmarinho@gmail.com>
2021-01-31 14:22:01 -0300

    feat: add one option input component

---
+++ src/components/InputOption/index.js
@@ -0,0 +1,57 @@
+import React, {useEffect, useRef, useState} from 'react';
+
+import {Text} from 'react-native';
+import {useField} from '@unform/core';
+
+import {BooleanInput, Title, Container} from './styles';
+
+function InputOption({name, field, ...rest}) {
+  const [selected, setSelected] = useState({value: null});
+  const inputRef = useRef(selected);
+
+  const {fieldName, registerField, defaultValue, error} = useField(name);
+
+  const onChange = (item) => {
+    inputRef.current = {value: item.value};
+    setSelected({value: item.value});
+  };
+
+  useEffect(() => {
+    inputRef.current = defaultValue;
+  }, [defaultValue]);
+
+  useEffect(() => {
+    registerField({
+      name: fieldName,
+      ref: inputRef.current,
+      path: 'value',
+      clearValue(ref) {
+        ref.clear();
+      },
+      getValue() {
+        return inputRef.current;
+      },
+    });
+  }, [fieldName, registerField]);
+
+  return (
+    <Container>
+      <Title>{field.title}</Title>
+      {field.choices.map((item) => (
+        <BooleanInput
+          key={item.value}
+          title={item.name}
+          ref={inputRef}
+          checked={selected.value === item.value}
+          onPress={() => onChange(item)}
+          checkedIcon="dot-circle-o"
+          uncheckedIcon="circle-o"
+          {...rest}
+        />
+      ))}
+      {error && <Text>{error}</Text>}
+    </Container>
+  );
+}
+
+export default InputOption;
