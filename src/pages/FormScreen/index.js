8cfaa127b2d4a1c8b8d5ffe72c23a48591f84042
BeAmarawn <bernardoamaralmarinho@gmail.com>
2021-02-01 06:56:19 -0300

    feat: form screen validations

--- src/pages/FormScreen/index.js
+++ src/pages/FormScreen/index.js
@@ -1,18 +1,47 @@
-import React, {useRef} from 'react';
-import {ScrollView} from 'react-native';
-import {Form} from '@unform/mobile';
+import React, { useRef } from 'react';
+import { ScrollView } from 'react-native';
+import { Form } from '@unform/mobile';
+import * as yup from 'yup';
 
-import {specificInputReturner} from '~/controller/FormTypeController';
+import { specificInputReturner } from '~/controller/FormTypeController';
+import { ValidationObjectBuilder } from '~/controller/ValidationObjectBuilder';
+import { createYupSchema } from '~/utils/yupSchemaCreator';
 
-import {Container, PrincipalText, ButtonText, EnterButton} from './styles';
+import { Container, PrincipalText, ButtonText, EnterButton } from './styles';
 
-const FormScreen = ({route, navigation}) => {
-  const {formData} = route.params;
+const FormScreen = ({ route, navigation }) => {
+  const { formData } = route.params;
   const formRef = useRef(null);
 
-  function handleSubmit(data) {
-    // console.tron.log(formData);
-    // console.tron.log(data);
+  async function handleSubmit(data) {
+    try {
+      const validationObject = formData.fields.map((item) =>
+        ValidationObjectBuilder(item)
+      );
+      const finalValidationObject = validationObject.filter(
+        (item) => item !== undefined
+      );
+      console.tron.log(finalValidationObject);
+      const yupSchema = finalValidationObject.reduce(createYupSchema, {});
+      const validateSchema = yup.object().shape(yupSchema);
+
+      await validateSchema.validate(data, {
+        abortEarly: false,
+      });
+      console.tron.log('passou', data);
+    } catch (err) {
+      const validationErrors = {};
+      console.tron.log('errorr', err);
+      if (err instanceof yup.ValidationError) {
+        err.inner.forEach((error) => {
+          validationErrors[error.path] = error.message;
+        });
+        formRef.current.setErrors(validationErrors);
+        console.tron.log('qef', validationErrors);
+      }
+    }
+
+    console.tron.log(data);
   }
 
   return (
@@ -21,7 +50,7 @@ const FormScreen = ({route, navigation}) => {
         <PrincipalText>{formData.title}</PrincipalText>
         <Form ref={formRef} onSubmit={handleSubmit}>
           {formData.fields.map((field) =>
-            specificInputReturner(field, field.name),
+            specificInputReturner(field, field.name)
           )}
           <EnterButton onPress={() => formRef.current.submitForm()}>
             <ButtonText>{formData.submitButtonText}</ButtonText>
