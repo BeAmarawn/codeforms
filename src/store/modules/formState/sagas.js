ae4805329e8c5ea941feadf36236936e26663f00
BeAmarawn <bernardoamaralmarinho@gmail.com>
2021-02-02 17:06:13 -0300

    feat: form progress state added to redux

--- src/store/modules/formState/sagas.js
+++ src/store/modules/formState/sagas.js
@@ -1,24 +1,23 @@
-import {Alert} from 'react-native';
-import {takeLatest, call, put, all} from 'redux-saga/effects';
+import { Alert } from 'react-native';
+import { takeLatest, call, put, all } from 'redux-saga/effects';
 
 import api from '../../../services/api';
-import * as RootNavigation from '~/RootNavigation';
 
-import {startSuccess, startFailure} from './actions';
+import { startSuccess, startFailure } from './actions';
 
-export function* start({payload}) {
+export function* start({ payload }) {
   try {
     const response = yield call(api.get, `/${payload.shortCode}`);
 
-    const started = response.data;
-    const {shortCode} = response.data;
-    RootNavigation.navigate('FormStart', {formData: response.data});
-    yield put(startSuccess(shortCode));
+    const formData = response.data;
+    const { shortCode } = response.data;
+
+    yield put(startSuccess(shortCode, formData));
   } catch (err) {
     if (err.response.status === 422) {
       Alert.alert(
         'Falha',
-        'O servidor não conseguiu completar sua requisição. Tente novamente',
+        'O servidor não conseguiu completar sua requisição. Tente novamente'
       );
 
       yield put(startFailure());
@@ -27,22 +26,17 @@ export function* start({payload}) {
     }
     Alert.alert(
       'Falha na autenticação do código',
-      'Houve um erro na requisição, verifique o código',
+      'Houve um erro na requisição, verifique o código'
     );
     yield put(startFailure());
   }
 }
 
-export function setShortCode({payload}) {
+export function setShortCode({ payload }) {
   if (!payload) return;
-}
-
-export function signOut() {
-  //
 }
 
 export default all([
   takeLatest('persist/REHYDRATE', setShortCode),
   takeLatest('@formState/START_REQUEST', start),
-  takeLatest('@auth/SIGN_OUT', signOut),
 ]);
